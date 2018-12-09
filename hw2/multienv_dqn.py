d71eb0c32e18d2ba3ad809faeedd5bb2ad7ef00f
kiddyboots216 <kiddyboots216@berkeley.edu>
2018-12-09 12:26:16 -0800

    added comm round

--- hw2/multienv_dqn.py
+++ hw2/multienv_dqn.py
@@ -36,6 +36,16 @@ def reset_adam(agent):
 if __name__ == "__main__":
     args = parser.parse_args()
     ray.init()
+    # Increasing the comm round length shouldn't just strictly make training better
+    if args.comm:
+        default_timesteps_per_iterations = args.timesteps_per_iteration
+        default_target_network_update_freq = args.target_network_update_freq
+        default_num_iters = args.num_iters
+        args.timesteps_per_iteration = int(default_num_iters * default_timesteps_per_iterations * args.comm)
+        args.target_network_update_freq = int(default_num_iters * default_target_network_update_freq * args.comm)
+        args.num_iters = int(default_num_iters * default_timesteps_per_iterations / args.timesteps_per_iteration)
+        print("Num-iters: {}".format(args.num_iters))
+        print("Timesteps: {}".format(args.timesteps_per_iteration))
 
     config = {"args": args}
 
