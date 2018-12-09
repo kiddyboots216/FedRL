5540d37661d1c43e70587666ad76953aba53a026
William Song <wsong@Appa.local>
2018-12-09 01:38:29 -0800

    add basic tune functionality, all results are still wrongly saved outside of directory

--- hw2/multienv_dqn.py
+++ hw2/multienv_dqn.py
@@ -26,53 +26,75 @@ parser.add_argument('--timesteps_per_iteration', '-tsteps', type=int, default=10
 parser.add_argument('--target_network_update_freq', '-target_freq', type=int, default=500)
 parser.add_argument('--env', '-e', type=str, default="CartPole-v0")
 
+def reset_adam(agent):
+    with a.local_evaluator.tf_sess.graph.as_default():
+        sess = a.local_evaluator.tf_sess
+        sess.run(a.reset_adam_optimizer)
+
 if __name__ == "__main__":
     args = parser.parse_args()
     ray.init()
 
-    agents = [DQNAgent(
-    env=args.env, 
-    config={
-        "gamma": 0.95,
-        "n_step": 3, # is this neccesary? Default is 1
-        "timesteps_per_iteration": args.timesteps_per_iteration,
-        "target_network_update_freq": args.target_network_update_freq,
-    }) for _ in range(args.num_agents)]
+    config = {"args": args}
 
-    new_weights = agents[0].get_weights(["default"]).get("default")
-    [a.set_weights({"default": new_weights}) for a in agents]
+    def FED_RL_BBY(config, reporter):
+        args = config["args"]
 
-    # add reset op for each agent    
-    for a in agents:
-        with a.local_evaluator.tf_sess.graph.as_default():
-            opt = a.local_evaluator.policy_map['default']._optimizer
-            a.reset_adam_optimizer = tf.variables_initializer(opt.variables())
-            
-    for i in range(args.num_iters):
-        print("== Iteration", i, "==")
-        results = []
-        # Improve each Agent
+        agents = [DQNAgent(
+        env=args.env,
+        config={
+            "gamma": 0.95,
+            "n_step": 3, # is this neccesary? Default is 1
+            "timesteps_per_iteration": args.timesteps_per_iteration,
+            "target_network_update_freq": args.target_network_update_freq,
+        }) for _ in range(args.num_agents)]
+
+        new_weights = agents[0].get_weights(["default"]).get("default")
+        [a.set_weights({"default": new_weights}) for a in agents]
+
+        # add reset op for each agent    
         for a in agents:
-            print("-- {} --".format(a._agent_name))
-            result = a.train()
-            print(pretty_print(result))
-            results.append(result)
-            # reset adam
             with a.local_evaluator.tf_sess.graph.as_default():
-                sess = a.local_evaluator.tf_sess
-                sess.run(a.reset_adam_optimizer)
+                opt = a.local_evaluator.policy_map['default']._optimizer
+                a.reset_adam_optimizer = tf.variables_initializer(opt.variables())
+                
+        for i in range(args.num_iters):
+            print("== Iteration", i, "==")
+            results = []
+            # Improve each Agent
+            for a in agents:
+                print("-- {} --".format(a._agent_name))
+                result = a.train()
+                print(pretty_print(result))
+                print("reporting results for:", a)
+                reporter(**result)
+                results.append(result)
+                # reset adam
+                if args.strategy != INDEPENDENT:
+                    reset_adam(a)
 
-        # if INDEPENDENT, agemnt weights do not need to be modified
-        if args.strategy != INDEPENDENT:
-            # gather all weights
-            all_weights = [a.get_weights(["default"]).get("default") for a in agents]
+            # if INDEPENDENT, agemnt weights do not need to be modified
+            if args.strategy != INDEPENDENT:
+                # gather all weights
+                all_weights = [a.get_weights(["default"]).get("default") for a in agents]
 
-            avg_returns = [result['episode_reward_mean'] for result in results]
-            if args.strategy == NAIVE:
-                new_weights = compute_average_weights(all_weights)
-            elif args.strategy == REWARD:
-                new_weights = compute_reward_weighted_avg_weights(all_weights, avg_returns)
-            elif args.strategy == MAX:
-                new_weights = compute_max_reward_weights(all_weights, avg_returns)
-            
-            [a.set_weights({"default": new_weights}) for a in agents]
+                avg_returns = [result['episode_reward_mean'] for result in results]
+                if args.strategy == NAIVE:
+                    new_weights = compute_average_weights(all_weights)
+                elif args.strategy == REWARD:
+                    new_weights = compute_reward_weighted_avg_weights(all_weights, avg_returns)
+                elif args.strategy == MAX:
+                    new_weights = compute_max_reward_weights(all_weights, avg_returns)
+                
+                [a.set_weights({"default": new_weights}) for a in agents]
+
+    configuration = tune.Experiment(
+        "experiment_name",
+        run=FED_RL_BBY,
+        trial_resources={"cpu": 2},
+        stop={},  
+        config=config
+    )
+
+    trials = tune.run_experiments(configuration, verbose=False)
+
