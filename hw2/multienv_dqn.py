65b6f6445e31fe87581a778106eb8088ad5e0d1e
David Vendrow <dvendrow@gmail.com>
2018-12-09 19:05:43 -0800

    logging

--- hw2/multienv_dqn.py
+++ hw2/multienv_dqn.py
@@ -14,13 +14,20 @@ from ray.rllib.agents.dqn.dqn_policy_graph import DQNPolicyGraph
 from ray.rllib.test.test_multi_agent_env import MultiCartpole
 from ray.tune.logger import pretty_print
 from ray.tune.registry import register_env
+from ray.tune.logger import UnifiedLogger
 from fed_utils import *
+
+from pathlib import Path
+import os
+import tempfile
+import time
+import uuid
 
 REWARD, NAIVE, INDEPENDENT, MAX = 'reward', 'naive', 'independent', 'max'
 CHOICES = [REWARD, NAIVE, INDEPENDENT, MAX]
 parser = argparse.ArgumentParser()
 parser.add_argument("--num_agents", '-na', type=int, default=2)
-parser.add_argument("--num_iters", '-iters', type=int, default=20)
+parser.add_argument("--num_iters", '-iters', type=int, default=2)
 parser.add_argument('--strategy', '-s', type=str, choices=CHOICES, default=INDEPENDENT)
 parser.add_argument('--timesteps_per_iteration', '-tsteps', type=int, default=1000)
 parser.add_argument('--target_network_update_freq', '-target_freq', type=int, default=500)
@@ -50,17 +57,42 @@ if __name__ == "__main__":
 
     config = {"args": args}
 
+    exp_name = "{}_{}_{}_{}_{}_{}_{}_{}_{}_{}".format(
+        args.name,
+        args.env,
+        args.num_agents,
+        args.num_iters,
+        args.strategy,
+        args.timesteps_per_iteration,
+        args.target_network_update_freq,
+        args.num_samples,
+        args.comm,
+        time.strftime("%Y-%m-%d_%H-%M-%S")
+    )
+
+
     def FED_RL(config, reporter):
         args = config["args"]
+        date_suffix = time.strftime("%Y-%m-%d_%H-%M-%S")
 
+        def logger_will(config):
+            path = Path.home() / "ray_results" / exp_name / "agents" / str(date_suffix)
+            logdir_prefix = "will"
+            if not os.path.exists(str(path)):
+                os.makedirs(str(path))
+            logdir = tempfile.mkdtemp(prefix=logdir_prefix, dir=str(path))
+            return UnifiedLogger(config, logdir, None)
+         
         agents = [DQNAgent(
-        env=args.env,
-        config={
-            "gamma": 0.95,
-            "n_step": 3, # is this neccesary? Default is 1
-            "timesteps_per_iteration": args.timesteps_per_iteration,
-            "target_network_update_freq": args.target_network_update_freq,
-        }) for _ in range(args.num_agents)]
+            env=args.env,
+            config={
+                "gamma": 0.95,
+                "n_step": 3, # is this neccesary? Default is 1
+                "timesteps_per_iteration": args.timesteps_per_iteration,
+                "target_network_update_freq": args.target_network_update_freq,
+            },
+            logger_creator=logger_will,
+        ) for _ in range(args.num_agents)]
 
         new_weights = agents[0].get_weights(["default"]).get("default")
         [a.set_weights({"default": new_weights}) for a in agents]
@@ -100,7 +132,7 @@ if __name__ == "__main__":
                     new_weights = compute_max_reward_weights(all_weights, avg_returns)
                 
                 [a.set_weights({"default": new_weights}) for a in agents]
-                reporter(avg_agent_reward=np.mean(avg_returns), std_agent_reward=np.std(avg_returns)
+                reporter(avg_agent_reward=np.mean(avg_returns), std_agent_reward=np.std(avg_returns))
 
     configuration = tune.Experiment(
         args.name,
@@ -108,7 +140,8 @@ if __name__ == "__main__":
         trial_resources={"cpu": 12},
         stop={},  
         config=config,
-        num_samples=args.num_samples
+        num_samples=args.num_samples,
+        local_dir=str(Path.home() / "ray_results" / exp_name)
     )
 
     trials = tune.run_experiments(configuration, verbose=False)
